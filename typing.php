723ac12933d3b1a77267e6cf4a99ef8c3006c7b1
utsumi <30435747+S-Isozaki@users.noreply.github.com>
2018-11-16 19:58:36 +0900

    Add files via upload

---
+++ typing.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html lang="ja">
+	<head>
+		<meta charset="utf-8">
+		<title>typing</title>
+	</head>
+
+	<body>
+		<h1>typing</h1>
+		
+		<h2>paste your favorite words here</h2>
+		
+		<br>
+		<input type="text">
+		</br>
+		<br>
+		<input type="button" value="start">
+		</br>
+		
+	</body>
+</html>
+
